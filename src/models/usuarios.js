69ba31e45ccaa099c12d173b6fbd68b5450f6bf1
GABBRIELJUA <20221040@uthh.edu.mx>
2024-11-19 19:49:39 -0600

    historipasscc

--- src/models/usuarios.js
+++ src/models/usuarios.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const bcryptjs = require('bcryptjs');
 
 const UsuarioSchema = new mongoose.Schema({
     nombre: {
@@ -42,44 +41,6 @@ const UsuarioSchema = new mongoose.Schema({
         type: Date,
         default: null 
     } ,
-
-    contrasenasAnteriores: [{ type: String }], // Array para contraseñas anteriores
 });
-
-
-// Método para comprobar si una contraseña ya fue usada
-usuarioSchema.methods.esContrasenaValida = async function (nuevaContrasena) {
-    // Comparar nueva contraseña con las contraseñas almacenadas
-    for (const contrasenaHash of this.contrasenasAnteriores) {
-      const coincide = await bcrypt.compare(nuevaContrasena, contrasenaHash);
-      if (coincide) {
-        return false; // La contraseña ya fue usada
-      }
-    }
-    return true; // La contraseña es válida
-  };
-  
-  // Método para actualizar la contraseña
-  usuarioSchema.methods.cambiarContrasena = async function (nuevaContrasena) {
-    const contrasenaValida = await this.esContrasenaValida(nuevaContrasena);
-    if (!contrasenaValida) {
-      throw new Error('La nueva contraseña no puede ser igual a una anterior.');
-    }
-  
-    // Hacer el hash de la nueva contraseña
-    const nuevaContrasenaHash = await bcryptjs.hash(nuevaContrasena, 10);
-    this.contrasena = nuevaContrasenaHash;
-  
-    // Almacenar la nueva contraseña en el array de contraseñas anteriores
-    this.contrasenasAnteriores.push(nuevaContrasenaHash);
-  
-    // Limitar el número de contraseñas anteriores (por ejemplo, 5)
-    if (this.contrasenasAnteriores.length > 5) {
-      this.contrasenasAnteriores.shift(); // Eliminar la más antigua
-    }
-  
-    await this.save(); // Guardar los cambios
-  };
-  
 
 module.exports = mongoose.model('Usuario', UsuarioSchema);

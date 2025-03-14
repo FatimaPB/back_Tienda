95c0682121cc05898eb6339df17755a3a054a028
GABBRIELJUA <20221040@uthh.edu.mx>
2025-03-14 01:52:20 -0600

    tur67fe5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const db = require('./src/config/db'); // Conexión a MySQL
+const mongoose = require('mongoose');
+const db = require('./src/config/db'); // Importar la conexión a MySQL
 const cors = require('cors');
+const https = require('https');
 const fs = require('fs');
 const cookieParser = require('cookie-parser');
 const UsuarioRoutes = require('./src/routes/usuarios');
@@ -10,38 +12,55 @@ const DocumentoRegulatorioRoutes = require('./src/routes/DocumentoRegulatorio');
 const TerminosycondicionesRoutes = require('./src/routes/Terminosycondiciones');
 const DeslindeRoutes = require('./src/routes/Deslinde');
 const EmpresaRoutes = require('./src/routes/Empresa');
-const authRoutes = require('./src/routes/auth');
+const authRoutes = require('./src/routes/auth')
 const limiteIntentosRoutes = require('./src/routes/limiteIntentosRoutes');
-const CategoriaRoutes = require('./src/routes/Cateoria'); // Revisa el nombre de tu archivo
+const CategoriaRoutes = require('./src/routes/Cateoria');
 const ColorRoutes = require('./src/routes/Color');
-const TamanosRoutes = require('./src/routes/Tamanos');
+const TamanosRoutes = require('./src/routes/Tamanos')
 const productosRoutes = require('./src/routes/productos');
 const bannerRoutes = require('./src/routes/banner');
 const nosotrosRoutes = require('./src/routes/nosotros');
-const metodoPagoRouter = require('./src/routes/metodos_pago');
+const metodoPagoRouter=require('./src/routes/metodos_pago');
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost', 'http://localhost:4200'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+    origin: ['http://localhost:3000', 'http://localhost', 'http://localhost:4200'], // Permite solo desde localhost
+    credentials: true,  // Permite el uso de cookies
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],  // Encabezados permitidos
+    preflightContinue: true,  // Permite continuar con la solicitud después de la prevalidación (opcional)
 };
 
-require('dotenv').config();
+
+
+require("dotenv").config();
 const app = express();
+
+// Rutas a los certificados generados
+const key = fs.readFileSync('C:/nginx-1.26.2/ssl/libreriacristorey.key', 'utf8');
+const cert = fs.readFileSync('C:/nginx-1.26.2/ssl/libreriacristorey.crt', 'utf8');
+
+// Configurar servidor HTTPS
+const server = https.createServer({ key, cert }, app);
+
+
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(cors(corsOptions));
+
+
+  
 app.use(cookieParser());
 
-app.use('/api', UsuarioRoutes);
-app.use('/api', loginRoutes);
-app.use('/api', DocumentoRegulatorioRoutes);
-app.use('/api', TerminosycondicionesRoutes);
-app.use('/api', DeslindeRoutes);
-app.use('/api', EmpresaRoutes);
-app.use('/api', authRoutes);
+app.use('/api',UsuarioRoutes);
+app.use('/api',loginRoutes);
+app.use('/api',DocumentoRegulatorioRoutes);
+app.use('/api',TerminosycondicionesRoutes);
+app.use('/api',DeslindeRoutes);
+app.use('/api',EmpresaRoutes);
+app.use('/api',authRoutes);
 app.use('/api', limiteIntentosRoutes);
 app.use('/api', CategoriaRoutes);
 app.use('/api', ColorRoutes);
@@ -51,28 +70,21 @@ app.use('/api', bannerRoutes);
 app.use('/api', nosotrosRoutes);
 app.use('/api', metodoPagoRouter);
 
-// Ruta de bienvenida
+
+//Rutas
 app.get("/", (req, res) => {
-  res.send("Servidor funcionando");
+    res.send("Servidor funcionando");
 });
 
 // Ruta de prueba para verificar la conexión a MySQL
 app.get("/test-db", (req, res) => {
-  db.query('SELECT NOW() as fecha_actual', (err, results) => {
-    if (err) {
-      console.error('❌ Error ejecutando la consulta:', err);
-      return res.status(500).json({ error: 'Error conectando a la base de datos' });
-    }
-    res.json({ mensaje: "✅ Conexión exitosa a MySQL db libreria", servidor_hora: results[0].fecha_actual });
-  });
+    db.query('SELECT NOW() as fecha_actual', (err, results) => {
+        if (err) {
+            console.error('❌ Error ejecutando la consulta:', err);
+            return res.status(500).json({ error: 'Error conectando a la base de datos' });
+        }
+        res.json({ mensaje: "✅ Conexión exitosa a MySQL db libreria", servidor_hora: results[0].fecha_actual });
+    });
 });
 
-// Si no se está ejecutando en el entorno de Vercel, inicia el servidor de forma local
-if (!process.env.VERCEL_ENV) {
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log(`🚀 Servidor escuchando en el puerto ${port}`);
-  });
-}
-
-module.exports = app;
+app.listen(port, () => console.log('servidor escuchando en el puerto', port));
